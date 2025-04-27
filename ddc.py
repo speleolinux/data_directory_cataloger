51c60e2f0a9697167d9154f40dd9da838fef4f17
speleolinux <mikel@speleonics.com.au>
2025-04-28 08:49:38 +1000

    Change to license

--- ddc.py
+++ ddc.py
@@ -22,19 +22,8 @@ such as MkDocs.
 To get help for using the program just run: ./ddc.py -h
 
 License
-
 Copyright 2022 Mike Lake
 
-This program is free software: you can redistribute it and/or modify it under the
-terms of the GNU General Public License as published by the Free Software Foundation,
-either version 3 of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE. See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with this
-program. If not, see <https://www.gnu.org/licenses/>.
 '''
 
 # Filename of the README in each directory that contains the directory meta information.
